820f3f262dbd95f2bc3762a67d7779f923ee9041
Varun <varunrawat343@gmail.com>
2024-05-24 01:04:39 +0530

    Add inline function

---
+++ inline.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+inline int square(int x) {return x*x;}
+
+int main() {
+        int x = 8;
+        cout << "Square of " << x << " is: " << square(x) << "\n";
+
+}
